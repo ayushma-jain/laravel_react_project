4cdbafc3788634cad1f71330529ac63ad671f7a9
ayushma-jain <ayushmaj46@gmail.com>
2024-08-03 00:42:56 +0530

    Backend

---
+++ backend/app/Http/Controllers/TodoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+       
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task' => 'required|string|max:255',
+            'completed_status' => 'boolean',
+        ]);
+
+        $todo = Todo::create($request->all());
+
+        return response()->json($todo, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return $todo;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'task' => 'string|max:255',
+            'complete_status' => 'boolean',
+        ]);
+
+        $todo->update($request->all());
+
+        return response()->json($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response()->json(null, 204);
+    }
+}
